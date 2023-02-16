d92dcfc095138b3b3f26db54dd411b92fec15bfc
Greg K <gkreisman_cep@qq.com>
2023-02-16 20:54:47 +0800

    add color

--- sketch2/sketch.js
+++ sketch2/sketch.js
@@ -5,6 +5,7 @@ let gsz = 100;
 let sz = 6;
 let img;
 let gen =1
+let clr = 90
 
 
 function setup() {
@@ -17,6 +18,7 @@ function setup() {
   //showGrid();
   //update();
   //showGrid();
+  colorMode(HSB,clr)
   frameRate(8);
   noStroke();
 }
@@ -67,58 +69,60 @@ function updateGrid() {
       //  8x2   
       //   4
       let n = 0 
-      if (grid[j-1][i] ==1) n+=1
-      if (grid[j][i+1]==1 ) n+=2
-      if (grid[j+1][i]== 1) n+=4
-      if (grid[j][i-1]==1 ) n+=8
+      if (grid[j-1][i] >0) n+=1
+      if (grid[j][i+1] >0) n+=2
+      if (grid[j+1][i]  >0) n+=4
+      if (grid[j][i-1] >0) n+=8
       // now you can make specific rules from 0 to 15
+      // live cells hold the generation in which they were born
       // ***** rules for live cells******
-      if (grid[j][i]==1) {
+      let cgs = grid[j][i]  // current grid state to pass on
+      if (grid[j][i] >0) {  // if alive
         switch(n){
           case 0:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 1:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 2:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 3:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 4:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 5:
             nextGrid[j][i] = 0;
             break;
           case 6:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 7:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 8:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 9:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 10:
             nextGrid[j][i] = 0;
             break;
           case 11:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 12:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = cgs;
             break;
           case 13:
-            nextGrid[j][i] = 0;
+            nextGrid[j][i] = cgs;
             break;
           case 14:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = 0;
             break;
           case 15:
             nextGrid[j][i] = 0;
@@ -126,22 +130,23 @@ function updateGrid() {
         }
       }
             // ***** rules for dead cells******
+            // now put in this generation
       if (grid[j][i]==0) {
         switch(n){
           case 0:
             nextGrid[j][i] = 0;
             break;
           case 1:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = gen;
             break;
           case 2:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = gen;
             break;
           case 3:
             nextGrid[j][i] = 0;
             break;
           case 4:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = gen;
             break;
           case 5:
             nextGrid[j][i] = 0;
@@ -153,7 +158,7 @@ function updateGrid() {
             nextGrid[j][i] = 0;
             break;
           case 8:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = gen;
             break;
           case 9:
             nextGrid[j][i] = 0;
@@ -174,7 +179,7 @@ function updateGrid() {
             nextGrid[j][i] = 0;
             break;
           case 15:
-            nextGrid[j][i] = 1;
+            nextGrid[j][i] = gen;
             break;
         }
       
@@ -194,9 +199,9 @@ function updateGrid() {
 function showGrid() {
   for (let j = 0; j < gsz; j++) {
     for (let i = 0; i < gsz; i++) {
-      if (grid[j][i] ==1){
+      if (grid[j][i] >0){  //is alive
         
-        fill(0,255,0)
+        fill(grid[j][i]%clr,300,300)
       }else{
       fill(0,0,0)
       }
